e39c343a47eca69419c6f0a07262069401ddb71a
AlexanderTitilin <lambdadelta89@gmail.com>
2021-12-02 21:33:14 +0300

    fix

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/usr/bash
+#!/bin/bash
 pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
@@ -6,7 +6,7 @@ makepkg -si
 cd 
 pip install --user pynvim
 yay -S autotiling ttf-nerd-fonts-hack-complete-git
-cp -r ~/backup-config/i3 ~/.backup-config
-cp ~/backup-config/.Xresources ~
+cp -r ~/backup-configs/i3 ~/.config
+cp ~/backup-configs/.Xresources ~
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
