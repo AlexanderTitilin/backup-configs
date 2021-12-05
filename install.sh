26339654d79a48deb587ccbfee4142cf3e7858f9
AlexanderTitilin <lambdadelta89@gmail.com>
2021-12-06 01:33:12 +0300

    fuck

--- install.sh
+++ install.sh
@@ -10,4 +10,6 @@ yay -S autotiling ttf-nerd-fonts-hack-complete-git
 cp -r ~/backup-configs/i3 ~/.config
 cp ~/backup-configs/.Xresources ~
 cp -r ~/backup-configs/nvim ~/.config 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 reboot
