16e510245bb4f967b3f48e39ff9ff2982c3e48e3
AlexanderTitilin <lambdadelta89@gmail.com>
2021-12-05 22:26:39 +0300

    add py3status and reboot

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -S --needed  base-devel
-sudo pacman -S zsh firefox neovim rxvt-unicode python python-pip
+sudo pacman -S zsh firefox neovim rxvt-unicode python python-pip py3status
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
@@ -10,4 +10,4 @@ yay -S autotiling ttf-nerd-fonts-hack-complete-git
 cp -r ~/backup-configs/i3 ~/.config
 cp ~/backup-configs/.Xresources ~
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+reboot
