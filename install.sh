f4b96d9273a7c79a25e0bdc3e08b4b3769ef8839
AlexanderTitilin <lambdadelta89@gmail.com>
2021-12-06 02:31:55 +0300

    add nitrogen

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -S --needed  base-devel
-sudo pacman -S zsh firefox neovim rxvt-unicode python python-pip py3status rofi zathura ranger pcmanfm ipython neofetch
+sudo pacman -S zsh firefox neovim rxvt-unicode python python-pip py3status rofi zathura ranger pcmanfm ipython neofetch nitrogen 
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
