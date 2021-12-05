093cbdc082f5901e4848ec651950b4b6cf62960a
AlexanderTitilin <lambdadelta89@gmail.com>
2021-12-05 22:05:34 +0300

    add some soft

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo pacman -S --needed git base-devel
-sudo pacman -S zsh
+sudo pacman -S --needed  base-devel
+sudo pacman -S zsh firefox neovim rxvt-unicode i3-scrot python python-pip
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
