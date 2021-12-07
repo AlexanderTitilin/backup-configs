8fee685426d49d17bc0a2bb186e76cffd7c627a7
AlexanderTitilin <lambdadelta89@gmail.com>
2021-12-07 18:24:45 +0300

    fix

--- install.sh
+++ install.sh
@@ -12,4 +12,5 @@ cp ~/backup-configs/.Xresources ~
 cp -r ~/backup-configs/nvim ~/.config 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 reboot
