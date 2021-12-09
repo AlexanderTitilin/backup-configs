770a35ff4f9f73218f570126846c1fdf26537065
AlexanderTitilin <lambdadelta89@gmail.com>
2021-12-09 19:42:44 +0300

    ...

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo pacman -S --needed  base-devel
-sudo pacman -S zsh firefox neovim rxvt-unicode python python-pip py3status rofi zathura ranger pcmanfm ipython neofetch nitrogen picom
+sudo pacman -S zsh firefox neovim python python-pip py3status rofi zathura ranger pcmanfm ipython neofetch nitrogen picom alacritty
+
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
@@ -8,7 +9,7 @@ cd
 pip install --user pynvim
 yay -S autotiling ttf-nerd-fonts-hack-complete-git
 cp -r ~/backup-configs/i3 ~/.config
-cp ~/backup-configs/.Xresources ~
+cp -r ~/backup-config/alacritty ~/.config/
 cp -r ~/backup-configs/nvim ~/.config 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
